adefaede0b7368b91924731c40adbb84965508cb
gongguopei <gongguopei87@gmail.com>
2015-08-14 17:13:42 +0800

    添加LoginActivity和RegisterActivity

--- app/src/main/java/net/pingfang/signalr/chat/activity/MainActivity.java
+++ app/src/main/java/net/pingfang/signalr/chat/activity/MainActivity.java
@@ -1,9 +1,9 @@
 package net.pingfang.signalr.chat.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 import net.pingfang.signalr.chat.R;
 
@@ -15,25 +15,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void login(View view) {
+        Intent intent = new Intent();
+        intent.setClass(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void register(View view) {
+        Intent intent = new Intent();
+        intent.setClass(getApplicationContext(),RegisterActivity.class);
+        startActivity(intent);
+        finish();
     }
+
+
 }
---
+++ app/src/main/java/net/pingfang/signalr/chat/activity/RegisterActivity.java
@@ -0,0 +1,17 @@
+package net.pingfang.signalr.chat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import net.pingfang.signalr.chat.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
