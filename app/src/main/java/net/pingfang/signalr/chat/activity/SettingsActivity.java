acb6f31788d3506e6e360c734d40ea4d778eecfc
gongguopei <gongguopei87@gmail.com>
2015-08-19 18:09:29 +0800

    更新关于项

--- app/src/main/java/net/pingfang/signalr/chat/activity/SettingsActivity.java
+++ app/src/main/java/net/pingfang/signalr/chat/activity/SettingsActivity.java
@@ -16,6 +16,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
     TextView btn_activity_back;
     TextView tv_settings_item_about;
+    TextView tv_settings_item_exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +31,8 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         btn_activity_back.setOnClickListener(this);
         tv_settings_item_about = (TextView) findViewById(R.id.tv_settings_item_about);
         tv_settings_item_about.setOnClickListener(this);
+        tv_settings_item_exit = (TextView) findViewById(R.id.tv_settings_item_exit);
+        tv_settings_item_exit.setOnClickListener(this);
     }
 
     @Override
@@ -43,6 +46,13 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 Intent intent = new Intent();
                 intent.setClass(getApplicationContext(),AppAboutActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.tv_settings_item_exit:
+                Intent exitIntent = new Intent();
+                exitIntent.setClass(getApplicationContext(),LoginActivity.class);
+                exitIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(exitIntent);
+                finish();
                 break;
         }
     }
